808444e8cf1738be54a506c5471bdba1905149c4
Idara Patrick <essienidara67@gmail.com>
2024-03-13 23:27:36 +0200

    correction 3

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,20 +1,19 @@
 #!/usr/bin/python3
-# Prints the number of and the list of its arguments
-
+#  Prints the number of and the list of its arguments
 if __name__ == "__main__":
     import sys
 
-    arguments = sys.argv
-    number_of_arguments = len(arguments) - 1
+    arg = sys.argv
+    size = len(arg) - 1
 
-    if number_of_arguments > 1:
-        print(f"{number_of_arguments} arguments:")
-        for i in range(1, number_of_arguments + 1):
-            print(f"{i}: {arguments[i]}")
+    if size > 1:
+        print("{} arguments:".format(size))
+        for i in range(1, size + 1):
+            print("{}: {}".format(i, arg[i]))
 
-    elif number_of_arguments == 0:
-        print(f"{number_of_arguments} arguments.")
+    elif size == 0:
+        print("{} arguments.".format(size))
 
     else:
-        print(f"{number_of_arguments} argument:")
-        print(f"{number_of_arguments}: {arguments[0]}")
+        print("{} argument:".format(size))
+        print("{}: {}".format(size, arg[1]))
