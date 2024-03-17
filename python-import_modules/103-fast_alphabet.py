75b831d62a2acbff1d067bb88122ac11ce32eec6
Idara Patrick <essienidara67@gmail.com>
2024-03-17 20:33:47 +0200

    h

--- python-import_modules/103-fast_alphabet.py
+++ python-import_modules/103-fast_alphabet.py
@@ -1,2 +1,2 @@
 #!/usr/bin/python3
-print(chr(65), chr(66), chr(67), chr(68), chr(69), chr(70), chr(71), chr(72), chr(73), chr(74), chr(75), chr(76), chr(77), chr(78), chr(79), chr(80), chr(81), chr(82), chr(83), chr(84), chr(85), chr(86), chr(87), chr(88), chr(89), chr(90), sep='')
+print(*(__import__('string').ascii_uppercase), sep='')
