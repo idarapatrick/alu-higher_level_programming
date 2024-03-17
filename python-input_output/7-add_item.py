e9ea0675e9c3f4dc2fa16d7d0c4e427cdd7309db
Idara Patrick <essienidara67@gmail.com>
2024-03-17 19:50:58 +0200

    file 7

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,14 +1,13 @@
 #!/usr/bin/python3
-"""adds all arguments to a Python list, and after that save them to a file:"""
-
-
+# 9-add_item.py
+"""Add all arguments to a Python list and save them to a file."""
 import sys
 
 if __name__ == "__main__":
     save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-    load_from_json_file = __import__('6-load_from_json_file') \
-        .load_from_json_file
-
+    load_from_json_file = \
+        __import__('6-load_from_json_file').load_from_json_file
+    
     try:
         loadFile = load_from_json_file("add_item.json")
     except FileNotFoundError:
