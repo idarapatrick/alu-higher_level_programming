3b9731da3772881c42676eecb1bd8f5dc9ce3fe3
Idara Patrick <essienidara67@gmail.com>
2024-03-17 19:26:37 +0200

    PEP 8

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -3,14 +3,17 @@
 
 import sys
 import json
-import os
+from os.path import exists
+from importlib import import_module
 
 if __name__ == "__main__":
+    save_to_json_file = import_module('5-save_to_json_file').save_to_json_file
+    load_from_json_file = import_module('6-load_from_json_file').load_from_json_file
+
     filename = "add_item.json"
 
     try:
-        with open(filename, 'r') as file:
-            load_file = json.load(file)
+        load_file = load_from_json_file(filename)
     except FileNotFoundError:
         load_file = []
 
@@ -18,5 +21,4 @@ if __name__ == "__main__":
     for idx in range(1, argc):
         load_file.append(sys.argv[idx])
 
-    with open(filename, 'w') as file:
-        json.dump(load_file, file)
+    save_to_json_file(load_file, filename)
