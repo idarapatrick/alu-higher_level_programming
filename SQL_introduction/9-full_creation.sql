1df36cbd6ee84792a96b09dbeddd6a583c09f4ab
Idara Patrick <essienidara67@gmail.com>
2024-03-21 16:28:18 +0200

    correction

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -3,10 +3,10 @@
 
 CREATE TABLE IF NOT EXISTS second_table
 (
-    id INT
-    name VARCHAR (256)
+    id INT,
+    name VARCHAR(256),
     score INT
-)
+);
 
 INSERT INTO second_table (id, name, score) VALUES
   (1, 'John', 10),
