defcd6c892dbeb4fd25fd000ebc66123ded6b976
Idara Patrick <essienidara67@gmail.com>
2024-03-21 16:25:20 +0200

    multiple rows correction

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -8,11 +8,8 @@ CREATE TABLE IF NOT EXISTS second_table
     score INT
 )
 
-INSERT INTO second_table(id, name score)
-VALUES(1, "John", 10)
-INSERT INTO second_table(id, name, score)
-VALUES(2, "Alex", 3)
-INSERT INTO second_table(id, name, score)
-VALUES(3, "Bob", 14)
-INSERT INTO second_table(id, namw, score)
-VALUES(4, "George", 8);
+INSERT INTO second_table (id, name, score) VALUES
+  (1, 'John', 10),
+  (2, 'Alex', 3),
+  (3, 'Bob', 14),
+  (4, 'George', 8);
