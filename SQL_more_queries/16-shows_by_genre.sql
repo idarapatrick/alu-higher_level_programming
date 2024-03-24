66cafedd2bd426c5515e4f341e716c131b5074f8
Idara Patrick <essienidara67@gmail.com>
2024-03-24 20:38:32 +0200

    shows by genre

--- SQL_more_queries/16-shows_by_genre.sql
+++ SQL_more_queries/16-shows_by_genre.sql
@@ -1,9 +1,5 @@
--- Script that lists genres of Dexter show
-SELECT tv_shows.title -- Query to join tables
-FROM tv_shows
-     JOIN tv_show_genres
-     	  ON tv_show_genres.show_id = tv_shows.id
-     JOIN tv_genres
-     	  ON tv_genres.id = tv_show_genres.genre_id
-WHERE tv_genres.name = "Comedy"
-ORDER BY tv_shows.title;
+-- Write a script that lists all shows, and all genres linked to that show, from the database hbtn_0d_tvshows
+SELECT tv_shows.title, tv_genres.name FROM tv_shows
+LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+LEFT JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
+ORDER BY tv_shows.title, tv_genres.name;
