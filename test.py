717ed61d3cc4fda3d772e602b8513a0f3bf9aeed
Idara Patrick <essienidara67@gmail.com>
2024-03-24 15:02:23 +0200

    more queries

---
+++ test.py
@@ -0,0 +1,11 @@
+
+
+
+
+my_tuple = ("apple", [1, 2, 3])
+
+print(my_tuple)  # Output: ("apple", [1, 2, 3])
+
+my_tuple[1].append(4)  # Modifying the list inside the tuple
+
+print(my_tuple)  # Output: ("apple", [1, 2, 3, 4]) (List inside tuple has changed)
