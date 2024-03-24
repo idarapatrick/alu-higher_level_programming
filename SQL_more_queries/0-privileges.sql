d7ffa38e2e4cccac177f1f2f0f696f08c7213493
Idara Patrick <essienidara67@gmail.com>
2024-03-24 19:54:28 +0200

    sql comment

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,3 +1,3 @@
-- Query to list all privileges (GRANT) of the MySQL users
+-- Query to list all privileges (GRANT) of the MySQL users
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
 SHOW GRANTS FOR 'user_0d_2'@'localhost';
