b73f7194cd4f09e172a6811fd5f301c28c237de9
idarapatrick <essienidara67@gmail.com>
2024-06-06 23:21:47 +0200

    semistandard validation check

--- javascript-warm_up/1-multi_languages.js
+++ javascript-warm_up/1-multi_languages.js
@@ -1,3 +1,3 @@
 #!/usr/bin/node
-const my_string = 'C is fun\nPython is cool\nJavaScript is amazing';
-console.log(my_string);
+const myString = 'C is fun\nPython is cool\nJavaScript is amazing';
+console.log(myString);
