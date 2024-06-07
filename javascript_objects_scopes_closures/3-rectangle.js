f3b3d5f7498aedc00256cdf7fe4070b1cf40949a
idarapatrick <essienidara67@gmail.com>
2024-06-07 23:08:12 +0200

    semistandard

--- javascript_objects_scopes_closures/3-rectangle.js
+++ javascript_objects_scopes_closures/3-rectangle.js
@@ -1,20 +1,20 @@
 #!/usr/bin/node
 
 module.exports = class Rectangle {
-    constructor (w, h) {
-      if (w > 0 && h > 0) {
-        this.width = w;
-        this.height = h;
-      }
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
     }
+  }
   
-    print () {
-      for (let i = 0; i < this.height; i++) {
+  print () {
+    for (let i = 0; i < this.height; i++) {
         let s = '';
         for (let j = 0; j < this.width; j++) {
           s += 'X';
         }
         console.log(s);
-      }
     }
+  }
 };
--- javascript_objects_scopes_closures/8-esrever.js
+++ javascript_objects_scopes_closures/8-esrever.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 
 exports.esrever = function (list) {
-    const result = [];
-    for (let i = list.length - 1; i >= 0; i--) {
-      result.push(list[i]);
+  const result = [];
+  for (let i = list.length - 1; i >= 0; i--) {
+    result.push(list[i]);
     }
-    return result;
+  return result;
 };
